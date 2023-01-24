d9edb4139dc3dd4b66a1aadca18dd0510b37b4cb
Manav Kulshrestha <manavkoolz@gmail.com>
2023-01-24 13:04:06 -0500

    removed web fiels

---
+++ lab1/client-server-sample/client.c
@@ -0,0 +1,50 @@
+// Client side C/C++ program to demonstrate Socket
+// programming
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 12000
+
+int main(int argc, char const* argv[])
+{
+	int sock = 0, valread, client_fd;
+	struct sockaddr_in serv_addr;
+	printf("Input lowercase sentence:\n");
+	char sentence[256];
+	fgets(sentence, sizeof(sentence), stdin);
+	char modifiedSentence[1024] = { 0 };
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Socket creation error \n");
+		return -1;
+	}
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+
+	// Convert IPv4 and IPv6 addresses from text to binary
+	// form
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
+		<= 0) {
+		printf(
+			"\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+
+	if ((client_fd
+		= connect(sock, (struct sockaddr*)&serv_addr,
+				sizeof(serv_addr)))
+		< 0) {
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+	send(sock, sentence, strlen(sentence), 0);
+	printf("Modified sentence received from server:\n");
+	valread = read(sock, modifiedSentence, 1024);
+	printf("%s\n", modifiedSentence);
+
+	// closing the connected socket
+	close(client_fd);
+	return 0;
+}
---
+++ prep.sh
@@ -0,0 +1,1 @@
+find . -size +30M | cat > .gitignore
